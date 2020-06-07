9db5be1699923c90c088b657d5ce2a9c8c788791
Radniel Padron <radnielpadron@gmail.com>
2020-06-07 00:18:26 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_it_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+  puts 'cool'
+end
+say_it_five_times
---
+++ learning-comments.rb
@@ -0,0 +1,2 @@
+# puts 'hey'
+puts 'meow'
